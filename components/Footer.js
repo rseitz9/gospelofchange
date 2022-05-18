ee5c73bb3f00d2761635ecc0e8f3c1a35c36ff59
Robert Seitz <robert.seitz@uber.com>
2022-05-18 12:04:25 +0200

    setup

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 const Footer = ({ googleDocId }) => (
   <div className="footer mt-8 flex flex-row justify-between items-center w-screen max-w-screen-md mx-auto p-3">
     <div>
-      <a href="https://regensunite.earth" title="Go back to regensunite.earth">
+      {/* <a href="https://regensunite.earth" title="Go back to regensunite.earth">
         <img
           src="/images/regensunite-favicon.png"
           alt="All for Climate Logo"
           className="h-10 mx-0"
         />
-      </a>
+      </a> */}
     </div>
     <div>
       <a
